216a2311c2bd2f5a65b9c373ea6eb7ff7b7c7f3b
yimmydiaz <yimmy.1701523504@ucaldas.edu.co>
2022-11-07 18:39:18 -0500

    comienzo curso

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { ContadorComponent } from './contador/contador.componet';
+
+
+@NgModule({
+    declarations:[
+        ContadorComponent
+    ],
+exports:[
+   ContadorComponent
+],
+imports:[
+    CommonModule
+]    
+
+
+})
+export class ContadorModule{
+
+}
---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent {
+
+  heroes: string[] = ['Spiderman', 'Iroman', 'Gocuk'];
+  heroeBorrado: string='';
+
+  borrarHeroe(){
+     this.heroeBorrado = this.heroes.shift() || '';
+  }
+}
